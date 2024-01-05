965217cb77881ce31f5bc8b2c1abe878c35c6613
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-01-05 12:10:24 +0700

    Add Users seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->admin()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+
+        User::factory(10)->create();
+    }
+}
