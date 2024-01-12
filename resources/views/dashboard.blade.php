e9f80759c2adb4c3a24493aa1db2483dfd39aa89
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-01-12 12:10:26 +0700

    Add translations for Dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout title="{{ Lang::get('main.titles.dashboard') }}">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ Lang::get('main.titles.dashboard') }}
         </h2>
     </x-slot>
 
@@ -9,14 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden p-6 shadow-sm sm:rounded-lg text-gray-900">
                 <p>
-                    {{ __("You're logged in!") }}
+                    {{ Lang::get('dashboard.login-confirmation') }}
                 </p>
-                @if (Auth::user()->email_verified_at === null)
-                    <p class="mt-2">
-                        {{ __("Please verify your email") }}
-                        <a href="{{ route('verification.notice') }}" class="text-blue-700 hover:text-blue-900">here</a>
-                    </p>
-                @endif
             </div>
         </div>
     </div>
