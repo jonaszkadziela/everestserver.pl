46780879762d5c8cfbf5b3cb8e4dd668da2a846e
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-01-21 19:23:31 +0100

    Add password.confirm middleware to all profile routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/privacy', fn () => view('privacy'))->name('privacy');
 Route::get('/dashboard', fn () => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/language/{code}', [LanguageController::class, 'change']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')->middleware('password.confirm');
+Route::middleware(['auth', 'password.confirm'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
