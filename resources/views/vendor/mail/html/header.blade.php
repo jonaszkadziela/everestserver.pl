02b840c16b103f50976410d2ca8cb5e4b33cf33b
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-01-22 22:47:39 +0100

    Change mail styles

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="{{ Vite::asset('resources/images/brand/everestserver-logo.svg') }}" alt="{{ Lang::get('home.logo-alt') }}" style="width: 64px;">
+<div>{{ $slot }}</div>
 </a>
 </td>
 </tr>
