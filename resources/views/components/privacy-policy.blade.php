973bb9c365629fcc75b500771e3f2404b1317701
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-18 22:43:35 +0100

    Move privacy policy into separate component

---
+++ resources/views/components/privacy-policy.blade.php
@@ -0,0 +1,132 @@
+@props(['lang'])
+
+<x-markdown {{ $attributes->merge(['class' => 'markdown']) }}>
+@if ($lang === 'en')
+...
+@else
+1. **INFORMACJE OGÓLNE**
+    1. Niniejsza polityka dotyczy Serwisu www, funkcjonującego pod adresem URL: [everestserver.pl](https://everestserver.pl).
+    2. Operatorem serwisu oraz Administratorem danych osobowych jest: jednoosobowa działalność gospodarcza "JONASZ KĄDZIELA" prowadzona przez Jonasza Kądzielę z siedzibą w Łodzi, ul. Orzechowa 11, 94-205, o numerze NIP 7272847808 oraz REGON 388094664.
+    3. Adres kontaktowy poczty elektronicznej operatora: [kontakt@jonaszkadziela.pl](mailto:kontakt@jonaszkadziela.pl).
+    4. Operator jest Administratorem Twoich danych osobowych w odniesieniu do danych podanych dobrowolnie w Serwisie.
+    5. Serwis wykorzystuje dane osobowe w następujących celach:
+        * prowadzenie systemu komentarzy,
+        * prezentacja profil użytkownika innym użytkownikom,
+        * obsługa zapytań przez formularz,
+        * realizacja zamówionych usług.
+    6. Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniu w następujący sposób:
+        * poprzez dobrowolnie wprowadzone w formularzach dane, które zostają wprowadzone do systemów Operatora,
+        * poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw. "ciasteczka").
+
+2. **WYBRANE METODY OCHRONY DANYCH STOSOWANE PRZEZ OPERATORA**
+    1. Miejsca logowania i wprowadzania danych osobowych są chronione w warstwie transmisji (certyfikat SSL). Dzięki temu dane osobowe i dane logowania, wprowadzone na stronie, zostają zaszyfrowane w komputerze użytkownika i mogą być odczytane jedynie na docelowym serwerze.
+    2. Hasła użytkowników są przechowywane w postaci hashowanej. Funkcja hashująca działa jednokierunkowo - nie jest możliwe odwrócenie jej działania, co stanowi obecnie współczesny standard w zakresie przechowywania haseł użytkowników.
+    3. W serwisie możliwa jest autentykacja dwuskładnikowa, co stanowi dodatkową formę ochrony logowania do Serwisu.
+    4. Operator okresowo zmienia swoje hasła administracyjne.
+    5. W celu ochrony danych Operator regularnie wykonuje kopie bezpieczeństwa.
+    6. Istotnym elementem ochrony danych jest regularna aktualizacja wszelkiego oprogramowania, wykorzystywanego przez Operatora do przetwarzania danych osobowych, co w szczególności oznacza regularne aktualizacje komponentów programistycznych.
+
+3. **HOSTING**
+    1. Serwis jest hostowany (technicznie utrzymywany) na serwerach zarządzanych przez Operatora.
+
+4. **TWOJE PRAWA I DODATKOWE INFORMACJE O SPOSOBIE WYKORZYSTANIA DANYCH**
+    1. W niektórych sytuacjach Administrator ma prawo przekazywać Twoje dane osobowe innym odbiorcom, jeśli będzie to niezbędne do wykonania zawartej z Tobą umowy lub do zrealizowania obowiązków ciążących na Administratorze. Dotyczy to takich grup odbiorców:
+        * upoważnieni pracownicy i współpracownicy, którzy korzystają z danych w celu realizacji celu działania strony.
+    2. Twoje dane osobowe przetwarzane przez Administratora nie dłużej, niż jest to konieczne do wykonania związanych z nimi czynności określonych osobnymi przepisami (np. o prowadzeniu rachunkowości). W odniesieniu do danych marketingowych dane nie będą przetwarzane dłużej niż przez 3 lata.
+    3. Przysługuje Ci prawo żądania od Administratora:
+        * dostępu do danych osobowych Ciebie dotyczących,
+        * ich sprostowania,
+        * usunięcia,
+        * ograniczenia przetwarzania,
+        * oraz przenoszenia danych.
+    4. Przysługuje Ci prawo do złożenia sprzeciwu wobec przetwarzania danych osobowych w celu wykonania prawnie uzasadnionych interesów realizowanych przez Administratora, w tym profilowania, przy czym prawo sprzeciwu nie będzie mogło być wykonane w przypadku istnienia ważnych prawnie uzasadnionych podstaw do przetwarzania, nadrzędnych wobec Ciebie interesów, praw i wolności, w szczególności ustalenia, dochodzenia lub obrony roszczeń.
+    5. Na działania Administratora przysługuje skarga do Prezesa Urzędu Ochrony Danych Osobowych, ul. Stawki 2, 00-193 Warszawa.
+    6. Podanie danych osobowych jest dobrowolne, lecz niezbędne do obsługi Serwisu.
+    7. W stosunku do Ciebie mogą być podejmowane czynności polegające na zautomatyzowanym podejmowaniu decyzji, w tym profilowaniu w celu świadczenia usług w ramach zawartej umowy oraz w celu prowadzenia przez Administratora marketingu bezpośredniego.
+    8. Twoje dane osobowe mogą być przekazywane podmiotom działającym w państwach trzecich, czyli poza terenem Unii Europejskiej. Ochronę w tym wypadku zapewnia zastosowanie standardowych klauzul ochrony danych przyjętych przez Komisję Europejską.
+
+5. **INFORMACJE W FORMULARZACH**
+    1. Serwis zbiera informacje podane dobrowolnie przez użytkownika, w tym dane osobowe, o ile zostaną one podane.
+    2. Serwis może zapisać informacje o parametrach połączenia (oznaczenie czasu, adres IP).
+    3. Serwis, w niektórych wypadkach, może zapisać informację ułatwiającą powiązanie danych w formularzu z adresem e-mail użytkownika wypełniającego formularz. W takim wypadku adres e-mail użytkownika pojawia się wewnątrz adresu URL strony zawierającej formularz.
+    4. Dane podane w formularzu są przetwarzane w celu wynikającym z funkcji konkretnego formularza, np. w celu dokonania procesu obsługi zgłoszenia serwisowego lub kontaktu handlowego, rejestracji usług itp. Każdorazowo kontekst i opis formularza w czytelny sposób informuje, do czego on służy.
+
+6. **LOGI ADMINISTRATORA**
+    1. Informacje zachowaniu użytkowników w serwisie mogą podlegać logowaniu. Dane te są wykorzystywane w celu administrowania serwisem.
+
+7. **ISTOTNE TECHNIKI MARKETINGOWE**
+    1. Operator stosuje analizę statystyczną ruchu na stronie poprzez Google Analytics. Operator nie przekazuje do operatora tej usługi danych osobowych, a jedynie zanonimizowane informacje. Usługa bazuje na wykorzystaniu ciasteczek w urządzeniu końcowym użytkownika. W zakresie informacji o preferencjach użytkownika gromadzonych przez sieć reklamową Google użytkownik może przeglądać i edytować informacje wynikające z plików cookies przy pomocy narzędzia: [Google My Ad Center](https://myadcenter.google.com).
+
+8. **INFORMACJA O PLIKACH COOKIES**
+    1. Serwis korzysta z plików cookies.
+    2. Pliki cookies (tzw. "ciasteczka") stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer.
+    3. Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator Serwisu.
+    4. Pliki cookies wykorzystywane są w następujących celach:
+        * utrzymanie sesji użytkownika Serwisu (po zalogowaniu), dzięki której użytkownik nie musi na każdej podstronie Serwisu ponownie wpisywać loginu i hasła,
+        * realizacji celów określonych powyżej w części "Istotne techniki marketingowe".
+    5. W ramach Serwisu stosowane są dwa zasadnicze rodzaje plików cookies: "sesyjne" (session cookies) oraz "stałe" (persistent cookies). Cookies "sesyjne" są plikami tymczasowymi, które przechowywane są w urządzeniu końcowym Użytkownika do czasu wylogowania, opuszczenia strony internetowej lub wyłączenia oprogramowania (przeglądarki internetowej). "Stałe" pliki cookies przechowywane są w urządzeniu końcowym Użytkownika przez czas określony w parametrach plików cookies lub do czasu ich usunięcia przez Użytkownika.
+    6. Oprogramowanie do przeglądania stron internetowych (przeglądarka internetowa) zazwyczaj domyślnie dopuszcza przechowywanie plików cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą dokonać zmiany ustawień w tym zakresie. Przeglądarka internetowa umożliwia usunięcie plików cookies. Możliwe jest także automatyczne blokowanie plików cookies. Szczegółowe informacje na ten temat zawiera pomoc lub dokumentacja przeglądarki internetowej.
+    7. Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre funkcjonalności dostępne na stronach internetowych Serwisu.
+    8. Pliki cookies zamieszczane w urządzeniu końcowym Użytkownika Serwisu wykorzystywane mogą być również przez współpracujące z operatorem Serwisu podmioty, w szczególności dotyczy to:
+        * Google Ireland Limited - dla usługi Google Analytics,
+        * Meta Platforms Ireland Limited - dla usługi Facebook Login.
+
+9. **ZARZĄDZANIE PLIKAMI COOKIES - JAK W PRAKTYCE WYRAŻAĆ I COFAĆ ZGODĘ?**
+    1. Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa, utrzymania preferencji użytkownika może utrudnić, a w skrajnych przypadkach może uniemożliwić korzystanie ze stron www.
+    2. W celu zarządzania ustawienia cookies wybierz z listy poniżej przeglądarkę internetową, której używasz i postępuj zgodnie z instrukcjami:
+        * [Google Chrome](https://support.google.com/chrome/answer/95647?hl=pl)
+        * [Mozilla Firefox](https://support.mozilla.org/pl/kb/wzmocniona-ochrona-przed-sledzeniem-firefox-desktop)
+        * [Microsoft Edge](https://support.microsoft.com/pl-pl/help/10607/microsoft-edge-view-delete-browser-history)
+        * [Microsoft Internet Explorer](https://support.microsoft.com/pl-pl/help/278835/how-to-delete-cookie-files-in-internet-explorer)
+        * [Apple Safari (macOS)](https://support.apple.com/kb/PH5042)
+
+        Urządzenia mobilne:
+        * [Google Chrome (Android)](https://support.google.com/chrome/answer/95647?hl=pl&co=GENIE.Platform%3DAndroid&oco=1)
+        * [Google Chrome (iOS iPhone)](https://support.google.com/chrome/answer/95647?hl=pl&co=GENIE.Platform%3DiOS&oco=1)
+        * [Mozilla Firefox Mobile (Android)](https://support.mozilla.org/pl/kb/jak-wyczyscic-ciasteczka-dane-witryny-pojedynczej-domeny-firefox-android)
+        * [Apple Safari (iOS)](https://support.apple.com/kb/HT1677?viewlocale=pl_PL)
+        * [Samsung Internet (Mobile)](https://www.samsung.com/uk/support/mobile-devices/what-are-cookies-and-how-do-i-enable-or-disable-them-on-my-samsung-galaxy-device)
+
+10. **ZMIANY POLITYKI PRYWATNOŚCI**
+    1. Operator zastrzega sobie prawo do wprowadzania zmian w Polityce Prywatności, jeśli wymagać tego będą przepisy prawa lub zmiany wprowadzane na stronach internetowych Operatora, jak również w celu usprawnienia działania Serwisu.
+    2. Data określona poniżej jest datą obowiązywania Polityki Prywatności w ostatniej wersji.
+
+        **Data ostatnich zmian: 22 stycznia 2023 r.**
+@endif
+</x-markdown>
+
+<style>
+.markdown {
+    text-align: left;
+}
+
+.markdown a {
+    color: #1d4ed8;
+}
+
+.markdown a:hover {
+    color: #1e3a8a;
+}
+
+.markdown ol {
+    list-style-type: decimal;
+    margin-left: 1rem;
+}
+
+.markdown > ol > li {
+    margin-bottom: 1rem;
+}
+
+.markdown > ol > li:last-child {
+    margin-bottom: 0;
+}
+
+.markdown > ol > li > ol {
+    margin-left: 2rem;
+}
+
+.markdown ul {
+    list-style-type: disc;
+    margin-left: 2rem;
+}
+</style>
