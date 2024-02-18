b8d838d3d653f3c7c10ab45bc9cfdf01c7d427fe
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-18 23:07:29 +0100

    Fix description translation for English privacy page

--- lang/en/privacy.php
+++ lang/en/privacy.php
@@ -12,7 +12,7 @@ return [
     */
 
     'title' => 'Privacy Policy',
-    'description' => 'The Privacy Policy describes the principles of our processing of information about you, including personal data and cookies, i.e. the so-called cookies',
+    'description' => 'The Privacy Policy describes the principles of our processing of information about you, including personal data and cookies',
     'contact' => 'In case of doubts regarding any of the provisions of this Privacy Policy, we are available at the e-mail address',
 
 ];
