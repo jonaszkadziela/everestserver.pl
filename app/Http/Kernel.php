d674eb487a0884b7eb31e1cbfa52beb5bd056cbc
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-23 22:00:23 +0100

    Add DetermineLanguageFromIp middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\DetermineLanguageFromIp;
 use App\Http\Middleware\EncryptCookies;
 use App\Http\Middleware\PreventRequestsDuringMaintenance;
 use App\Http\Middleware\RedirectIfAuthenticated;
@@ -59,6 +60,7 @@ class Kernel extends HttpKernel
             ShareErrorsFromSession::class,
             VerifyCsrfToken::class,
             SubstituteBindings::class,
+            DetermineLanguageFromIp::class,
             SetLanguage::class,
         ],
 
