be7063419ed702744752f7855cb08050ccb64af0
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-24 17:45:37 +0100

    Change mail logo to PNG format for better compatibility with email clients

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ Vite::asset('resources/images/brand/everestserver-logo.svg') }}" alt="{{ Lang::get('home.logo-alt') }}" style="width: 64px;">
+<img src="{{ Vite::asset('resources/images/brand/everestserver-logo.png') }}" alt="{{ Lang::get('home.logo-alt') }}" style="width: 64px;">
 <div>{{ $slot }}</div>
 </a>
 </td>
