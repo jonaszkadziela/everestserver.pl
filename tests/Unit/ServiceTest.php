fcb4378a655008195e832daf0234cdd234f07a70
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-25 22:23:33 +0100

    Add unit test for Service model

---
+++ tests/Unit/ServiceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Service;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class ServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private array $attributes;
+    private Service $service;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->attributes = [
+            'name' => 'EverestService',
+            'description' => 'Sample service on EverestServer',
+            'icon' => 'fa-server fa-solid',
+            'link' => 'https://service.everestserver.test',
+            'is_public' => true,
+            'is_enabled' => true,
+            'created_at' => Carbon::parse('2024-01-20 14:05:35'),
+            'updated_at' => Carbon::parse('2024-01-20 16:05:35'),
+        ];
+
+        $this->service = Service::factory()->create($this->attributes);
+    }
+
+    public function test_service_attributes(): void
+    {
+        $this->assertSame($this->attributes['name'], $this->service->name);
+        $this->assertSame($this->attributes['description'], $this->service->description);
+        $this->assertSame($this->attributes['icon'], $this->service->icon);
+        $this->assertSame($this->attributes['link'], $this->service->link);
+        $this->assertSame($this->attributes['is_public'], $this->service->is_public);
+        $this->assertSame($this->attributes['is_enabled'], $this->service->is_enabled);
+        $this->assertTrue($this->attributes['created_at']->equalTo($this->service->created_at));
+        $this->assertTrue($this->attributes['updated_at']->equalTo($this->service->updated_at));
+    }
+
+    public function test_service_casts(): void
+    {
+        $this->assertTrue(getType($this->service->is_public) === 'boolean');
+        $this->assertTrue(getType($this->service->is_enabled) === 'boolean');
+        $this->assertTrue($this->service->created_at instanceof Carbon);
+        $this->assertTrue($this->service->updated_at instanceof Carbon);
+    }
+
+    public function test_service_traits(): void
+    {
+        $this->assertTrue(in_array(HasFactory::class, class_uses($this->service)));
+    }
+
+    public function test_service_parent_classes(): void
+    {
+        $this->assertTrue($this->service instanceof \Illuminate\Database\Eloquent\Model);
+    }
+}
