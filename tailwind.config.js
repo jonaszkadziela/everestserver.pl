99603e7228c7b1a33912927c83ba46d31746a6ae
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-02-25 22:43:05 +0100

    Change Tailwind CSS config to look for classes in migrations directory and remove unused font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,17 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
 import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './app/View/Components/**/*.php',
+        './database/migrations/**/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './storage/framework/views/*.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+    theme: {},
 
     plugins: [forms],
 }
