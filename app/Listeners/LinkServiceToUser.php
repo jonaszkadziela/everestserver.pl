e9cd26dde885d6ef9c99a150dd6a243a43703261
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-05-26 16:47:08 +0200

    Add LinkServiceToUser event listener

---
+++ app/Listeners/LinkServiceToUser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Laravel\Passport\Client;
+use Laravel\Passport\Events\AccessTokenCreated;
+use Laravel\Passport\Token;
+
+class LinkServiceToUser
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(AccessTokenCreated $event): void
+    {
+        $eventArray = [
+            'clientId' => $event->clientId,
+            'tokenId' => Str::substr($event->tokenId, 0, 5) . '...',
+            'userId' => $event->userId,
+        ];
+
+        $oAuthClient = Client::find($event->clientId);
+        $user = User::find($event->userId);
+
+        if ($oAuthClient === null || $user === null) {
+            Log::info(get_class($this) . ': ' . ($user === null ? 'User' : 'OAuth Client') . ' not found', $eventArray);
+            return;
+        }
+
+        $service = Service::where('name', '=', $oAuthClient->name)->first();
+
+        if ($service === null) {
+            Log::info(get_class($this) . ': Service not found', $eventArray);
+            return;
+        }
+
+        $oAuthAccessToken = Token::find($event->tokenId);
+
+        if ($oAuthAccessToken === null) {
+            Log::info(get_class($this) . ': OAuth Access Token not found', $eventArray);
+            return;
+        }
+
+        $linkedService = $user
+            ->services()
+            ->where('service_id', '=', $service->id)
+            ->where('identifier', '=', $oAuthAccessToken->name)
+            ->first();
+
+        if ($linkedService !== null) {
+            Log::debug(get_class($this) . ': Service already linked', $eventArray);
+            return;
+        }
+
+        $user->services()->save($service, ['identifier' => $oAuthAccessToken->name]);
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use App\Listeners\LinkServiceToUser;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Laravel\Passport\Events\AccessTokenCreated;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+        AccessTokenCreated::class => [
+            LinkServiceToUser::class,
+        ],
+
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
