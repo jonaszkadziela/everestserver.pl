aba46633237f1a2b1ea3e9f1d1b9646078132684
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-06-05 23:41:33 +0200

    Add password input component

---
+++ resources/views/components/password-input.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'disabled' => false,
+    'class' => '',
+])
+
+<div class="border border-gray-300 flex focus-within:ring-2 focus-within:ring-blue-700 p-0 rounded-md shadow-sm {{ $class }}"
+     x-data="{ show: false }"
+>
+    <input style="box-shadow: unset;"
+           x-bind:type="show ? 'text' : 'password'"
+           {!! $attributes->merge(['class' => 'border-0 focus:outline-0 rounded-l-md w-full']) !!}
+           {{ $disabled ? 'disabled' : '' }}
+    >
+    <button class="px-2"
+            type="button"
+            @click="show = !show"
+    >
+        <i class="fa-fw fa-regular" x-bind:class="show ? 'fa-eye' : 'fa-eye-slash'"></i>
+    </button>
+</div>
