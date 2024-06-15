13d460c81f04bf6e27411bdda0b49552084f727b
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-06-15 21:52:27 +0200

    Improve ServiceController so that EverestPass accounts can be linked

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Service;
 use App\Models\User;
+use App\Services\EverestPass\EverestPassClient;
 use App\View\Components\Notification;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class ServiceController extends Controller
 {
     /**
      * Link service to user account.
+     *
+     * @throws AuthenticationException
+     * @throws RequestException
+     * @throws RuntimeException
      */
     public function link(Request $request): RedirectResponse
     {
@@ -24,39 +33,77 @@ class ServiceController extends Controller
 
         /** @var User $user */
         $user = Auth::user();
-        $service = Service::enabled()
-            ->where('id', '=', $request->serviceId)
-            ->when(!$user?->is_admin, fn (Builder $query) => $query->where('is_public', '=', true))
-            ->first();
+        $pivotAttributes = [];
 
-        if ($user === null || $service === null) {
+        try {
+            $service = Service::enabled()
+                ->where('id', '=', $request->serviceId)
+                ->when(!$user?->is_admin, fn (Builder $query) => $query->where('is_public', '=', true))
+                ->firstOrFail();
+
+            $redirectLink = match ($service->name) {
+                'EverestCloud' => $service->link . '/apps/sociallogin/custom_oauth2/everestserver',
+                'EverestGit' => $service->link . '/-/profile/account',
+                default => null,
+            };
+
+            if ($redirectLink !== null) {
+                return redirect($redirectLink);
+            }
+
+            if ($service->name === 'EverestPass') {
+                $response = $this->findOrCreateEverestPassAccount($service, $user);
+                $pivotAttributes = [
+                    'identifier' => $user->email,
+                ];
+
+                Log::info(
+                    get_class($this) . (Str::startsWith($response['header']['message'], 'The user was successfully added')
+                        ? ': EverestPass account created' : ': EverestPass account found'),
+                    $response['header'],
+                );
+            }
+
+            $user->services()->save($service, $pivotAttributes);
+
+            Notification::push(
+                Lang::get('notifications.in-app.service-linked.title'),
+                Lang::get('notifications.in-app.service-linked.description', ['service' => $service->name]),
+                Notification::SUCCESS,
+            );
+        } catch (Exception $e) {
             Notification::push(
                 Lang::get('notifications.in-app.service-link-failed.title'),
                 Lang::get('notifications.in-app.service-link-failed.description'),
                 Notification::DANGER,
             );
 
-            return redirect()->back();
+            Log::info(
+                get_class($this) . ': ' . $service->name . ' service could not be linked to user ' . $user->username,
+                [$e->getMessage()],
+            );
         }
-
-        $redirectLink = match ($service->name) {
-            'EverestCloud' => $service->link . '/apps/sociallogin/custom_oauth2/everestserver',
-            'EverestGit' => $service->link . '/-/profile/account',
-            default => null,
-        };
-
-        if ($redirectLink !== null) {
-            return redirect($redirectLink);
-        }
-
-        $user->services()->save($service);
-
-        Notification::push(
-            Lang::get('notifications.in-app.service-linked.title'),
-            Lang::get('notifications.in-app.service-linked.description', ['service' => $service->name]),
-            Notification::SUCCESS,
-        );
 
         return redirect()->back();
+    }
+
+    /**
+     * Find or create EverestPass account.
+     *
+     * @throws AuthenticationException
+     * @throws RequestException
+     * @throws RuntimeException
+     */
+    private function findOrCreateEverestPassAccount(Service $service, User $user): array
+    {
+        $client = new EverestPassClient($service->link);
+
+        $response = $client->getUsers($user->email);
+
+        if (count($response['body']) === 0) {
+            $response = $client->createUser($user->email, $user->username, ' ');
+        }
+
+        return $response;
     }
 }
