fded1e325ecf064142ea931effb249827bc96cac
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-06-21 17:29:57 +0200

    Add AccountCreatedViaCommand notification

--- app/Notifications/AccountCreatedViaCommand.php
+++ app/Notifications/AccountCreatedViaCommand.php
@@ -6,24 +6,24 @@ use App\Notifications\Traits\HasTranslations;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AccountCreatedViaProvider extends Notification
+class AccountCreatedViaCommand extends Notification
 {
     use HasTranslations;
 
     protected array $lang = [
         'en' => [
             'action' => 'Log in your account',
-            'line-1' => 'Welcome to EverestServer! An account has been created for you via **:provider**.',
-            'line-2' => 'We automatically generated the **:password** password for you. For safety reasons, please change it as soon as possible.',
-            'line-3' => 'You can access your account via **:provider** or with **:email** email address and the password mentioned above.',
+            'line-1' => 'Welcome to EverestServer! An account has been created for you by the administrator.',
+            'line-2' => 'We generated the **:password** password for you. For safety reasons, please change it as soon as possible.',
+            'line-3' => 'You can access your account using the **:email** email address and the password mentioned above.',
             'line-4' => 'If you did not create an account, you can ignore this message.',
             'subject' => 'EverestServer - Account created',
         ],
         'pl' => [
             'action' => 'Zaloguj się na swoje konto',
-            'line-1' => 'Witamy w EverestServer! Utworzono dla Ciebie konto za pośrednictwem **:provider**.',
-            'line-2' => 'Automatycznie wygenerowaliśmy dla Ciebie hasło **:password**. Ze względów bezpieczeństwa prosimy o jak najszybszą zmianę.',
-            'line-3' => 'Możesz uzyskać dostęp do swojego konta poprzez **:provider** lub za pomocą adresu e-mail **:email** i hasła wymienionego powyżej.',
+            'line-1' => 'Witamy w EverestServer! Twoje konto zostało utworzone przez administratora.',
+            'line-2' => 'Wygenerowaliśmy dla Ciebie hasło **:password**. Ze względów bezpieczeństwa prosimy o jak najszybszą zmianę.',
+            'line-3' => 'Możesz uzyskać dostęp do swojego konta za pomocą adresu e-mail **:email** i hasła wymienionego powyżej.',
             'line-4' => 'Jeśli nie utworzyłeś konta, zignoruj tę wiadomość.',
             'subject' => 'EverestServer - Utworzono konto',
         ],
@@ -32,10 +32,9 @@ class AccountCreatedViaProvider extends Notification
     /**
      * Create a notification instance.
      */
-    public function __construct(private string $provider, private string $email, private string $password)
+    public function __construct(private string $email, private string $password)
     {
         $this->placeholdersMap = [
-            ':provider' => $provider,
             ':email' => $email,
             ':password' => $password,
         ];
