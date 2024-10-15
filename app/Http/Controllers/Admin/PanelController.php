a0e36ac6424bd79d475647df3605caed365323d9
Jonasz Kądziela <contact@jonaszkadziela.pl>
2024-10-15 22:47:35 +0200

    Refactor PanelController to avoid duplicate database queries

--- app/Http/Controllers/Admin/PanelController.php
+++ app/Http/Controllers/Admin/PanelController.php
@@ -42,38 +42,41 @@ class PanelController extends Controller
      */
     private function getData(Request $request, string $tab): array
     {
-        return match ($tab) {
-            'services' => [
-                'raw' => (new ServiceController())->index($request),
-                'transformed' => (new ServiceController())
-                    ->index($request)
-                    ->getCollection()
-                    ->transform(function (Service $service) {
-                        return [
-                            ...$service->getAttributes(),
-                            'description' => $service->description,
-                            'is_public' => $service->is_public ? Lang::get('main.yes') : Lang::get('main.no'),
-                            'is_enabled' => $service->is_enabled ? Lang::get('main.yes') : Lang::get('main.no'),
-                        ];
-                    })
-                    ->toArray(),
-            ],
-            'users' => [
-                'raw' => (new UserController())->index($request),
-                'transformed' => (new UserController())
-                    ->index($request)
-                    ->getCollection()
-                    ->transform(function (User $user) {
-                        return [
-                            ...$user->getAttributes(),
-                            'email_verified_at' => $user->email_verified_at?->toDateTimeString() ?? '-',
-                            'is_admin' => $user->is_admin ? Lang::get('main.yes') : Lang::get('main.no'),
-                            'is_enabled' => $user->is_enabled ? Lang::get('main.yes') : Lang::get('main.no'),
-                        ];
-                    })
-                    ->toArray(),
-            ],
+        $raw = match ($tab) {
+            'services' => (new ServiceController())->index($request),
+            'users' => (new UserController())->index($request),
             default => [],
         };
+
+        $transformed = match ($tab) {
+            'services' => $raw
+                ->getCollection()
+                ->map(function (Service $service) {
+                    return [
+                        ...$service->getAttributes(),
+                        'description' => $service->description,
+                        'is_public' => $service->is_public ? Lang::get('main.yes') : Lang::get('main.no'),
+                        'is_enabled' => $service->is_enabled ? Lang::get('main.yes') : Lang::get('main.no'),
+                    ];
+                })
+                ->toArray(),
+            'users' => $raw
+                ->getCollection()
+                ->map(function (User $user) {
+                    return [
+                        ...$user->getAttributes(),
+                        'email_verified_at' => $user->email_verified_at?->toDateTimeString() ?? '-',
+                        'is_admin' => $user->is_admin ? Lang::get('main.yes') : Lang::get('main.no'),
+                        'is_enabled' => $user->is_enabled ? Lang::get('main.yes') : Lang::get('main.no'),
+                    ];
+                })
+                ->toArray(),
+            default => [],
+        };
+
+        return [
+            'raw' => $raw,
+            'transformed' => $transformed,
+        ];
     }
 }
